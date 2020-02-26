6b81ef054370c93d54abe40789f791d64bf9e079
Air Max <testmax200@gmail.com>
2020-02-26 10:39:25 +0100

    new

--- tsbasics/exports.js
+++ tsbasics/exports.js
@@ -4,3 +4,7 @@ var classDemo_1 = require("./classDemo");
 var airmax = new classDemo_1.classDemo();
 airmax.setSSN();
 console.log(airmax.getSSN());
+function name(params) {
+    console.log(params);
+}
+name(2345);
--- tsbasics/exports.ts
+++ tsbasics/exports.ts
@@ -3,3 +3,10 @@ import {classDemo} from './classDemo';
 let airmax = new classDemo();
 airmax.setSSN();
 console.log(airmax.getSSN());
+
+
+function name(params:number) {
+    console.log(params);
+}
+
+name(2345);
